1fde1c18feeeeee81c2339e46bf95f6b03c7caab
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2020-10-28 17:01:11 +0530

    Django install 3.1.2

---
+++ e_library/e_library/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
+from django.urls import path
+
+urlpatterns = i18n_patterns(
+    path('admin/', admin.site.urls),
+    prefix_default_language=False
+)
