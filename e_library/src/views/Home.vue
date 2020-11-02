c22a509330cbfe24b48dddab5dace1c7e1f5e5d7
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2020-11-02 21:44:56 +0530

    Bhagwan Hai Kahan Re Tu

--- e_library/src/views/Home.vue
+++ e_library/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
     <h1>This is Home</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <ol v-for="b in book" :key="b.id">
+          <li>
+            {{ b.first_name }} {{ b.last_name }}
+          </li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    book: [],
+    messag: "Api work proper"
+  },
+  created:function(){
+    console.log("Created Home API")
+    fetch('/system/Books/').then(
+      res => console.log(res.data)
+    ).then(
+      data => book = data
+    ).catch(
+      err => console.error('Error: ',err)
+    )
   }
 }
 </script>
---
+++ e_library/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    outputDir: 'management',
+    assetsDir: 'static',
+    indexPath: 'templates/management/index.html',
+    // productionSourceMap: false,
+    filenameHashing: false,
+}
