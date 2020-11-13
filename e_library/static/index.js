331bc7f4c12b234dd88d0ab5e446a25412c2e9e5
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2020-11-13 23:55:44 +0530

    Happy :happy:

--- e_library/static/index.js
+++ e_library/static/index.js
@@ -1,18 +1,30 @@
-const vuetify = new Vuetify({
-theme: {
-dark: false,
-},
-icons: {
-iconfont: "md",
-},
-});
+Start();
+function Start() {
+	const vuetify = new Vuetify({
+		theme: {
+			dark: localStorage.getItem('library_dark'),
+			options: {
+				themeCache: {
+					get(){return localStorage.getItem('library_dark')},
+					set(){
+						return localStorage.setItem('library_dark', !localStorage.getItem('library_dark') || false)
+					},
+				},
+			}
+		},
+		icons: {
+			iconfont: "md",
+		},
+	});
 
-new Vue({
-el: "#app",
-delimiters: ["${", "}"],
-data: () => ({
-drawer: false,
-name: "{{ user }}",
-}),
-vuetify,
-});
+	new Vue({
+		el: "#app",
+		delimiters: ["${", "}"],
+		data: () => ({
+			drawer: false,
+			name: "Test Name Vue",
+		}),
+		vuetify,
+	});
+}
+
