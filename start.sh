97fa3d092e17cb891ddf050efd087ce7e9f1ff62
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2021-01-09 01:03:45 +0530

    find my Image

--- start.sh
+++ start.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/bash
+
+printf "$(tput bold)hy, $(tput setaf 6)$USERNAME$(tput sgr0)"
+function safe_c() {
+    if pgrep python; 
+    then 
+        pkill python
+        printf "\n$(tput bold)$(tput setaf 1)Python Exit....." 
+    fi
+    printf "\n\n$(tput bold)$(tput setaf 1)Bye Bye 👋$(tput bel)\n"
+}
+
+trap safe_c SIGINT
+
+python backend/manage.py runserver 0.0.0.0:8000 &
+cd frontend && npm start
